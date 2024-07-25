e865a9f5b4e0dc477f9a511ef3d0ac71ed365093
e.mittendorfer <elisabeth.mitten@gmail.com>
2024-07-25 18:53:42 +0200

    create player movement

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import time
 from turtle import Screen
 
+from player import Player
+
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Py Turtle Crossing")
 screen.tracer(0)
+
+player = Player()
+
+screen.listen()
+screen.onkeypress(player.move_up, "Up")
 
 game_is_on = True
 while game_is_on:
---
+++ player.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("green")
+        self.penup()
+        self.setheading(90.0)
+        self.goto(STARTING_POSITION)
+
+    def move_up(self):
+        print(self.position())
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(0, new_y)
